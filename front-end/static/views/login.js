7126df47f62f31a5a62b3165f9e1da9539bc6e47
sachinrao2811 <sachinrao94450@gmail.com>
2022-10-11 20:49:25 +0530

    error resolve

--- front-end/static/views/login.js
+++ front-end/static/views/login.js
@@ -25,45 +25,34 @@ const login = {
 
     function loginfun () {
       const token = document.getElementById("inputToken").value;
-      // const url = "https://www.eventbriteapi.com/v3/users/me/organizations/";
       console.log(apiUrls.loginUrl);
 
-      try {
-        fetch(apiUrls.loginUrl, {
-          method: "get",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json"
-          }
-        }).then(res => {
-          if (res.ok) {
-            alert("successfully");
-          } else {
-            alert("Something went wrong");
-          };
-        }).then((data) => {
+      fetch(apiUrls.loginUrl, {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        }
+      })
+        .then((response) => response.json())
+        .then((data) => {
           localStorage.setItem("token", token);
           localStorage.setItem("orgId", data.organizations[0].id);
           window.location.href = `/dashboard?useid=${data.organizations[0].id}`;
         });
-      } catch (error) {
-        console.error(error);
-        // expected output: ReferenceError: nonExistentFunction is not defined
-        // Note - error messages will vary depending on browser
-      }
 
-    //   fetch(apiUrls.loginUrl, {
-    //     method: "get",
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //       "Content-Type": "application/json"
-    //     }
-    //   }).then(response => response.json()).then((data) => {
-    //     localStorage.setItem("token", token);
-    //     localStorage.setItem("orgId", data.organizations[0].id);
-    //     window.location.href = `/dashboard?useid=${data.organizations[0].id}`;
-    //   });
-    };
+      //   fetch(apiUrls.loginUrl, {
+      //     method: "get",
+      //     headers: {
+      //       Authorization: `Bearer ${token}`,
+      //       "Content-Type": "application/json"
+      //     }
+      //   }).then(response => response.json()).then((data) => {
+      //     localStorage.setItem("token", token);
+      //     localStorage.setItem("orgId", data.organizations[0].id);
+      //     window.location.href = `/dashboard?useid=${data.organizations[0].id}`;
+      //   });
+    }
   }
 };
 
